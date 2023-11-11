f005bfdf100bc487da7b476b2c2820ae169c39c3
JamesLawwd <jleo56291@gmail.com>
2023-11-11 12:16:07 +0300

    completed assignment

--- src/data/items.js
+++ src/data/items.js
@@ -1,3 +1,6 @@
+import react,{useState} from "react";
+
+
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
   { id: 2, name: "Pomegranate", category: "Produce" },
@@ -5,5 +8,23 @@ const items = [
   { id: 4, name: "String Cheese", category: "Dairy" },
   { id: 5, name: "Cookies", category: "Dessert" },
 ];
+
+function Item({ name, category }) {
+  const[isincart,setincart]=useState(false)
+  const[action,setAction]=useState(true)
+
+  function handleAddToCart(){
+    setincart(!isincart)
+    setAction(!action)
+  }
+  return (
+    <li className={isincart? "in-cart":""}>
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button className="add" onClick={handleAddToCart}>{action?"Add to Cart":"Remove from Cart"}</button>
+    </li>
+  );
+}
+
 
 export default items;
